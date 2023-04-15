d5cbdc30fda47b92246aaf937c36080d2ae78f7d
Minh Bui <buiducminh287@gmail.com>
2023-04-15 10:19:02 +0700

    Add main.py file

---
+++ main.py
@@ -0,0 +1,9 @@
+from mainlib.storage import Storage
+from mainlib.locations import LocationCrawler
+
+if __name__ == "__main__":
+    storage = Storage("foobar.db")
+    crawler = LocationCrawler(storage)
+    crawler._crawl_first_level()
+    crawler._crawl_other_level(level=2)
+    crawler._crawl_other_level(level=3)
