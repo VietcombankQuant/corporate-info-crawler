d0a9de4857912fa36e9fbd339609c1bd495a1d08
Mibu287 <buiducminh287@gmail.com>
2023-04-22 14:08:47 +0700

    Rename main.py to __main__.py

--- __main__.py
+++ __main__.py
@@ -4,7 +4,11 @@ import sqlalchemy
 from crawler.region import RegionCrawler
 
 if __name__ == "__main__":
-    db_path = pathlib.Path("output") / "corporate-info.sqlite3.db"
+    output_path = pathlib.Path("output")
+    if not output_path.exists():
+        output_path.mkdir()
+    db_path = output_path / "corporate-info.sqlite3.db"
     storage_engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
+
     crawler = RegionCrawler(storage_engine)
     crawler.crawl()
