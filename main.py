3dfbbca38e70800880637f518f4e5820f3e9607a
Mibu287 <buiducminh287@gmail.com>
2023-04-22 13:10:20 +0700

    Change location of output database to output directory

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sqlalchemy
 from crawler.region import RegionCrawler
 
 if __name__ == "__main__":
-    db_path = pathlib.Path(".") / "foobar.db"
+    db_path = pathlib.Path("output") / "corporate-info.sqlite3.db"
     storage_engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
     crawler = RegionCrawler(storage_engine)
     crawler.crawl()
