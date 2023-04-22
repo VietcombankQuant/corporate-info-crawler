045eec539e6ffb7b9d0fafe0d4936291fe93abb2
Mibu287 <buiducminh287@gmail.com>
2023-04-22 15:36:19 +0700

    Add function configure_logger in file crawler/common.py

--- crawler/common.py
+++ crawler/common.py
@@ -1,4 +1,29 @@
 from sqlalchemy.orm import declarative_base
+from loguru import logger
+import pathlib
+import sys
+
+__all__ = ["BASE_URL", "SqlTableBase"]
 
 BASE_URL = "wkajyoa4n5.execute-api.ap-southeast-1.amazonaws.com"
 SqlTableBase = declarative_base()
+
+
+def configure_logger():
+    logger_format = (
+        "<green>{time}</green> | "
+        "<level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+        "<level>{message}</level>"
+    )
+
+    log_path = pathlib.Path.cwd() / "output" / "logs"
+    log_path.mkdir(parents=True, exist_ok=True)
+    log_file = log_path / "log_{time}.txt"
+
+    logger.remove()
+    logger.add(sys.stdout, format=logger_format)
+    logger.add(log_file, format=logger_format, rotation="64 MB", enqueue=True)
+
+
+configure_logger()
