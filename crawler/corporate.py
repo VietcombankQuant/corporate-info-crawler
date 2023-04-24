3e1280819cb35ba533bb9b53e7eb1c858081715c
Mibu287 <buiducminh287@gmail.com>
2023-04-24 19:01:19 +0700

    Extract corporate information from source url

--- crawler/corporate.py
+++ crawler/corporate.py
@@ -36,6 +36,9 @@ class Corporate(SqlTableBase):
     def create_table(Self, engine: sqlalchemy.Engine):
         Self.metadata.create_all(engine)
 
+    def __repr__(self) -> str:
+        return f'Tax ID: "{self.tax_id}", Name: "{self.name}"'
+
 
 @dataclass
 class SearchResult:
@@ -53,49 +56,72 @@ class CorporateCrawler:
         with SqlSession(self.storage_engine) as session:
             query = session.query(Region).where(Region.level == 3)
             regions = [region for region in query]
+            logger.success(
+                f"Got {len(regions)} regions at level 3 from database"
+            )
 
         cookie_jar = aiohttp.DummyCookieJar()
         async with aiohttp.ClientSession(cookie_jar=cookie_jar) as client:
             for region in regions:
                 urls = await self._search_by_region(client, region)
                 tasks = [
-                    self._extract_corporate_info(client, url) for url in urls
+                    self._extract_corporate_info(client, url, region) for url in urls
                 ]
                 corporates = await asyncio.gather(*tasks)
 
+                record_counter = 0
                 with SqlSession(self.storage_engine) as sql_session:
                     for corporate in corporates:
-                        if corporate != None and sql_session.get(Corporate, ident=corporate.tax_code) != None:
+                        if corporate != None and sql_session.get(Corporate, ident=corporate.tax_id) == None:
                             sql_session.add(corporate)
+                            record_counter += 1
+                    sql_session.commit()
+
+                logger.success(
+                    f'Added {record_counter} corporate infor records '
+                    f'in region {region.id} - {region} into "corporates table"'
+                )
+
+    _corporate_xpath_queries = {
+        "tax_id": '//table[@class = "table-taxinfo"]//td[@itemprop="taxID"]/span/text()',
+        "name": '//table[@class = "table-taxinfo"]//th[@itemprop="name"]/span/text()',
+        "rep_person": '//table[@class = "table-taxinfo"]//td/span[@itemprop="name"]/a/text()',
+        "company_type": '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-building")]/parent::td/following-sibling::td/a/text()',
+        "industry": '//h3[contains(text(), "Ngành nghề kinh doanh")]//following-sibling::table//td/strong/a/text()',
+        "address": '//table[@class = "table-taxinfo"]//td[@itemprop="address"]/span/text()',
+        "phone": '//table[@class = "table-taxinfo"]//td[@itemprop="telephone"]/span/text()',
+        "active_date": '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-calendar")]/parent::td/following-sibling::td/span/text()',
+        "status": '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-info")]/parent::td/following-sibling::td/a/text()',
+        "last_update": '//table[@class = "table-taxinfo"]//button[@data-target = "#modal-update"]/preceding-sibling::em/text()',
+    }
 
     async def _extract_corporate_info(self, client: aiohttp.ClientSession, url: str, region: Region) -> Union[Corporate, None]:
-        international_name = SqlColumn(SqlString)
-        short_name = SqlColumn(SqlString)
+        # Fetch corporate data from url
+        full_url = f"https://{BASE_URL}{url}"
+        async with client.get(full_url) as resp:
+            if not resp.ok:
+                logger.error(
+                    f"Failed to get corporate data from {url} with status {resp.status}"
+                )
+                return None
+            content = await resp.text()
 
-        queries = {
-            "tax_id": '//table[@class = "table-taxinfo"]//td[@itemprop="taxID"]/span/text()',
-            "name": '//table[@class = "table-taxinfo"]//th[@itemprop="name"]/span/text()',
-            "rep_person": '//table[@class = "table-taxinfo"]//td/span[@itemprop="name"]/a/text()',
-            "company_type": '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-building")]/parent::td/following-sibling::td/a/text()',
-            "industry": '//h3[contains(text(), "Ngành nghề kinh doanh")]//following-sibling::table//td/strong/a/text()',
-            "address": '//table[@class = "table-taxinfo"]//td[@itemprop="address"]/span/text()',
-            "phone": '//table[@class = "table-taxinfo"]//td[@itemprop="telephone"]/span/text()',
-            "active_date": '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-calendar")]/parent::td/following-sibling::td/span/text()',
-            "status": '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-info")]/parent::td/following-sibling::td/a/text()',
-            "last_update": '//table[@class = "table-taxinfo"]//button[@data-target = "#modal-update"]/preceding-sibling::em/text()',
-        }
+        # Extract data from response
+        document = etree.HTML(content)
+        corporate = Corporate()
+        corporate.region_id = region.id
 
-        tax_id = '//table[@class = "table-taxinfo"]//td[@itemprop="taxID"]/span/text()'
-        name = '//table[@class = "table-taxinfo"]//th[@itemprop="name"]/span/text()'
-        rep_person = '//table[@class = "table-taxinfo"]//td/span[@itemprop="name"]/a/text()'
-        company_type = '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-building")]/parent::td/following-sibling::td/a/text()'
-        industry = '//h3[contains(text(), "Ngành nghề kinh doanh")]//following-sibling::table//td/strong/a/text()'
-        address = '//table[@class = "table-taxinfo"]//td[@itemprop="address"]/span/text()'
-        phone = '//table[@class = "table-taxinfo"]//td[@itemprop="telephone"]/span/text()'
-        active_date = '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-calendar")]/parent::td/following-sibling::td/span/text()'
-        region_id = region.id
-        status = '//table[@class = "table-taxinfo"]//td/i[contains(@class, "fa-info")]/parent::td/following-sibling::td/a/text()'
-        last_update = '//table[@class = "table-taxinfo"]//button[@data-target = "#modal-update"]/preceding-sibling::em/text()'
+        for field_name, query in self._corporate_xpath_queries.items():
+            results = document.xpath(query)
+            if len(results) != 0:
+                field_value = " ".join(results)
+                corporate.__dict__[field_name] = field_value
+
+        logger.success(
+            f"Extracted corporate information item {corporate} from {url}"
+        )
+
+        return corporate
 
     async def _search_by_region(self, client: aiohttp.ClientSession, region: Region) -> set[str]:
         search_url = f"https://{BASE_URL}{region.url}"
