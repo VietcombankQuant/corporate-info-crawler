d6a1c4ee9987ae3e93d81c657a4c7f04b1daf667
Mibu287 <buiducminh287@gmail.com>
2023-05-14 12:21:59 +0700

    Fix bug RetryClient.__aenter__ not return self

--- crawler/retry_client.py
+++ crawler/retry_client.py
@@ -1,6 +1,8 @@
 import aiohttp
 import asyncio
+from contextlib import asynccontextmanager
 
+from .common import logger
 from .ratelimit import RateLimiter
 
 
@@ -11,29 +13,38 @@ class RetryClient:
         self.args = args
         self.kwargs = kwargs
 
+    @asynccontextmanager
     async def get(self, url, *args, **kwargs) -> aiohttp.ClientResponse:
         for i in range(self.max_retries):
             async with self.limiter as _:
                 async with self.session.get(url, *args, **kwargs) as resp:
-                    if resp.ok:
-                        return resp
-                    timeout = 2**i
-                    await asyncio.sleep(timeout)
-        return resp
+                    if resp.ok or (i == self.max_retries - 1):
+                        yield resp
+                        return
+            logger.warning(
+                f"Retry {i+1}/{self.max_retries} for {url} failed with status {resp.status}"
+            )
+            timeout = 2**i
+            await asyncio.sleep(timeout)
 
+    @asynccontextmanager
     async def post(self, url, *args, **kwargs) -> aiohttp.ClientResponse:
         for i in range(self.max_retries):
             async with self.limiter as _:
                 async with self.session.post(url, *args, **kwargs) as resp:
-                    if resp.ok:
-                        return resp
-                    timeout = 2**i
-                    await asyncio.sleep(timeout)
-        return resp
+                    if resp.ok or (i == self.max_retries-1):
+                        yield resp
+                        return
+            logger.warning(
+                f"Retry {i+1}/{self.max_retries} for {url} failed with status {resp.status}"
+            )
+            timeout = 2**i
+            await asyncio.sleep(timeout)
 
     async def __aenter__(self):
-        self.session = aiohttp.ClientSession(*self.args, **self.kwargs)
-        return await self.session.__aenter__()
+        session = aiohttp.ClientSession(*self.args, **self.kwargs)
+        self.session = await session.__aenter__()
+        return self
 
     async def __aexit__(self, *args, **kwargs):
         await self.session.__aexit__(*args, **kwargs)
